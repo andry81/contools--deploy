8c3af3430689fdb3dcd644761210beb0e2e13090
andry81 <andry@inbox.ru>
2017-06-22 23:03:38 +0000

    * scripts update
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/deploy/trunk@5 c36ca5be-e84b-4a79-b306-48ffd5d22b39

--- contools/git/contools_git.sync_all.bat
+++ contools/git/contools_git.sync_all.bat
@@ -1,5 +1,11 @@
 @echo off
 
+setlocal
+
+if "%NEST_LVL%" == "" set NEST_LVL=0
+
+set /A NEST_LVL+=1
+
 pushd "%~dp0contools_git" && (
   call :CMD git pull origin master || ( popd & goto EXIT )
   call :CMD git subtree pull --prefix=Scripts/Tools tools master || ( popd & goto EXIT )
@@ -8,7 +14,10 @@ pushd "%~dp0contools_git" && (
 )
 
 :EXIT
-pause
+set /A NEST_LVL-=1
+
+if %NEST_LVL% LEQ 0 pause
+
 exit /b
 
 :CMD
