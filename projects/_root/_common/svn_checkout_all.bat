56a26d7bb169a744d098eb1bbdaa08665b78a75d
andry81 <andry@inbox.ru>
2018-01-19 17:46:45 +0000

    * missed change
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/deploy/trunk@34 c36ca5be-e84b-4a79-b306-48ffd5d22b39

--- projects/_root/_common/svn_checkout_all.bat
+++ projects/_root/_common/svn_checkout_all.bat
@@ -13,16 +13,20 @@ call "%%CONFIGURE_ROOT%%\configure.user.bat" || goto :EOF
 set "?~n0=%~n0"
 set "?~nx0=%~nx0"
 
-set "OP_NAME=%?~n0:*~=%"
+set "OP_NAME=%?~n0%"
+set "HUB_ABBR=%~1"
+set "VAR_PREFIX=%~2"
 
-set "HUB_ABBR=%?~n0%"
-if "%OP_NAME%" == "%?~n0%" goto IGNORE_HUB_ABBR_SUFFIX
-call set "HUB_ABBR=%%HUB_ABBR:%OP_NAME%=%%"
-set "HUB_ABBR=%HUB_ABBR:~0,-1%"
+if not defined HUB_ABBR (
+  echo.%?~nx0%: error: HUB_ABBR is not set.
+  exit /b 1
+) >&2
+if not defined VAR_PREFIX (
+  echo.%?~nx0%: error: VAR_PREFIX is not set.
+  exit /b 2
+) >&2
 
-:IGNORE_HUB_ABBR_SUFFIX
-set "PROJECTS_ROOT=%~dp0"
-if "%PROJECTS_ROOT:~-1%" == "\" set "PROJECTS_ROOT=%PROJECTS_ROOT:~0,-1%"
+set "PROJECTS_ROOT=%CONFIGURE_ROOT%"
 
 rem from leaf repositories to a root repository
 call set "%VAR_PREFIX%.PROJECT_PATH_LIST=%%%VAR_PREFIX%.PROJECT_PATH_LIST:{{HUB_ABBR}}=%HUB_ABBR%%%"
