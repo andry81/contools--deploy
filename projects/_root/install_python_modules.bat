68095481196ab0d41e0cabec6ccb6c8d051ddbc3
andry81 <andry@inbox.ru>
2019-11-13 23:13:29 +0000

    2019.11.14: * fixed: tacklelib: avoid copy builtin and packaged modules * fixed: tacklelib: avoid copy builtin and non trivial class types * fixed: tacklelib: `inspect.getmodule` is replaced by `tkl_get_imported_module_by_file_path` to correctly return modules by a stack module path (unique relation) instead by a module name from `sys.modules` (not unique relation) * changed: python modules install script update * refactor: `tkl_import_module` function reduction * refactor: code cleanup
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/deploy/trunk@92 c36ca5be-e84b-4a79-b306-48ffd5d22b39

--- projects/_root/install_python_modules.bat
+++ projects/_root/install_python_modules.bat
@@ -1,11 +1,12 @@
 @echo off
 
-set "PYTHON_EXE_PATH=c:/python/x86/37/python.exe"
+set "PYTHON_EXE_PATH=c:/python/x86/38/python.exe"
 
 call :CMD "%%PYTHON_EXE_PATH%%" -m pip install pip --upgrade || goto EXIT
+call :CMD "%%PYTHON_EXE_PATH%%" -m pip install setuptools --upgrade || goto EXIT
 call :CMD "%%PYTHON_EXE_PATH%%" -m pip install win_unicode_console --upgrade || goto EXIT
 call :CMD "%%PYTHON_EXE_PATH%%" -m pip install prompt-toolkit --upgrade || goto EXIT
-rem call :CMD "%%PYTHON_EXE_PATH%%" -m pip install xonsh --upgrade || goto EXIT
+call :CMD "%%PYTHON_EXE_PATH%%" -m pip install xonsh --upgrade || goto EXIT
 call :CMD "%%PYTHON_EXE_PATH%%" -m pip install plumbum --upgrade || goto EXIT
 call :CMD "%%PYTHON_EXE_PATH%%" -m pip install pyyaml --upgrade || goto EXIT
 call :CMD "%%PYTHON_EXE_PATH%%" -m pip install conditional --upgrade || goto EXIT
